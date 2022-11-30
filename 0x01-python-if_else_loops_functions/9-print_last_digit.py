5b4fd19b37482e3500b7c65fabbf1ab2b54a7f94
Shugo52 <olofuchubi@gmail.com>
2022-11-29 16:44:41 -0800

    correct 9 + 10-add.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print("{}".format((int(number) * -1) % 10 if int(number) < 0 else int(number) % 10), end=(""))
+    print("{}".format((int(number) * -1) % 10 if int(number) < 0
+          else int(number) % 10), end=(""))
     return ((int(number) * -1) % 10 if int(number) < 0 else int(number) % 10)
