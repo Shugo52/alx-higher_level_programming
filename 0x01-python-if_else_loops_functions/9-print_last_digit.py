40b4b6a958a90b89487528e018f54da185f9e9b6
Shugo52 <olofuchubi@gmail.com>
2022-11-29 16:40:57 -0800

    9- print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    print("{}".format((int(number) * -1) % 10 if int(number) < 0 else int(number) % 10), end=(""))
+    return ((int(number) * -1) % 10 if int(number) < 0 else int(number) % 10)
