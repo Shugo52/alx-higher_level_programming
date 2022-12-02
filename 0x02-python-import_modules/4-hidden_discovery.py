efb4d9fb229fb509b4859e4ded6a1bfdb7b88f14
Shugo52 <olofuchubi@gmail.com>
2022-12-01 19:19:22 -0800

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
     names = [name for name in dir(hidden_4) if name[:2] != "__"]
     for name in names:
         print(name)
-
