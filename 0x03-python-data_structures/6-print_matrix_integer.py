19958715b017c1cb34fb5624bec92cd635ce5cac
Shugo52 <olofuchubi@gmail.com>
2022-12-05 20:00:15 -0800

    6 - update fo checker

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in range(len(matrix)):
             for j in matrix[i]:
-                print("{:d}".format(j), end=" "
-                      if matrix[i].index(j) <
-                      len(matrix[i]) - 1 else "\n")
+                print("{:d}".format(j), end="")
+                if matrix[i].index(j) != (len(matrix[i]) - 1):
+                    print(" ", end="")
+            print()
