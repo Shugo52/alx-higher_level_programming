7ba5b61ebec2c49ffd19c75d4d9958cf01e702c1
Shugo52 <olofuchubi@gmail.com>
2022-12-07 13:48:03 -0800

    2- updated

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    return sum([my_list[i] if my_list[i]
-           not in my_list[:i] else 0 for i in range(len(my_list))])
+def uniq_add(my_list=[]) -> int:
+    return sum(set(my_list))
