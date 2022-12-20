051ca7c0de539adb2fe19ecd917a362a1520a967
Ojoachubione  Olofu <olofuchubi@gmail.com>
2022-12-20 02:23:25 +0100

    Task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = list()
+    for i in range(list_length):
+        try:
+            quotient = my_list_1[i] / my_list_2[i]
+        except (TypeError, ValueError):
+            print("wrong type")
+            quotient = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            quotient = 0
+        except IndexError:
+            print("out of range")
+            quotient = 0
+        finally:
+            new_list.append(quotient)
+    return new_list
+
+
+if __name__ == "__main__":
+    safe_print_list()
