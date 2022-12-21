f522c668e6c2a383d704ef1313d32478d77489f6
Ojoachubione  Olofu <olofuchubi@gmail.com>
2022-12-21 02:08:38 +0100

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""Defines a Square class."""
+
+
+class Square():
+    """Defines a Square class."""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__init__(value)
+
+    def area(self):
+        """Calculates the area of a square"""
+
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints a square to stdout"""
+
+        if self.__size:
+            for _ in range(self.__size):
+                print("#" * self.__size)
+        else:
+            print()
