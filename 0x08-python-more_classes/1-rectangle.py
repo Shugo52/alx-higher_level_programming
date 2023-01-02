a0160660822134598aa550e0dc49b6f59231038a
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-01-02 23:57:45 +0100

    corrected string literal

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -26,10 +26,10 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if type(value) is not int:
-            raise TypeError(width must be an integer)
+            raise TypeError("width must be an integer")
 
         if value < 0:
-            raise ValueError(width must be >= 0)
+            raise ValueError("width must be >= 0")
 
         self.__width = value
 
@@ -40,9 +40,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if type(value) is not int:
-            raise TypeError(height must be an integer)
+            raise TypeError("height must be an integer")
 
         if value < 0:
-            raise ValueError(height must be >= 0)
+            raise ValueError("height must be >= 0")
 
         self.__height = value
