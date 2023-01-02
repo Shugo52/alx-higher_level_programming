9f8555ee7d227b69994a45e2ba0c0de4eaa396b8
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-01-02 23:53:31 +0100

    Task 1 - create a rectangle class with private attributes with both having property and setter decorators

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""The Rectangle module
+
+Makes available a Rectangle class for rectangle representation
+"""
+
+
+class Rectangle:
+    """Creates an instance of a Rectangle
+
+    Args:
+        None
+
+    Attributes:
+        Rectangle object/instance
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError(width must be an integer)
+
+        if value < 0:
+            raise ValueError(width must be >= 0)
+
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError(height must be an integer)
+
+        if value < 0:
+            raise ValueError(height must be >= 0)
+
+        self.__height = value
