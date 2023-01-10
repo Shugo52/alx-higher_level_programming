cf151bbbadbd27b1af938e73622c372bcf830669
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-01-10 05:42:38 +0100

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Base geometry module."""
+
+
+class BaseGeometry:
+    """Geometry class."""
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
