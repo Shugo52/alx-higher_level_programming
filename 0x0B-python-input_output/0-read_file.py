36603cee6297a8e8a7d43a01808af8b6783c08ba
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-01-11 04:03:19 +0100

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""Read File Module"""
+"""Read File Module."""
 
 
 def read_file(filename=""):
-	with open(filename, 'r', encoding="utf-8") as f:
-		print(f.read(), end="")
+    """Prints a textfile to stdout.
+
+    Args:
+        filename: File to read contents.
+
+    Returns:
+        Number of characters read from <filename>."""
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end="")
 
 
 if __name__ == "__main__":
-	read_file()
+    read_file()
