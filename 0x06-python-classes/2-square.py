2ffcc64e71092a90581600ff4b2de2803eb1c477
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-02-04 14:32:10 +0100

    Task 2 update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,8 +7,8 @@ class Square():
     """Defines a Square class."""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        if int is not type(size):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
