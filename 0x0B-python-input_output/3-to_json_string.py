79fe93df94892673eed2daa6c9784bc81aa29849
Olofu Ojoachubione <87946002+Shugo52@users.noreply.github.com>
2023-02-08 07:40:27 +0100

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,4 +4,13 @@ import json
 
 
 def to_json_string(my_obj):
+    """Represents an object in JSON format.
+
+    Args:
+        my_obj: Object to parse to JSON representation.
+
+    Returns:
+        JSON representation of an object.
+    """
+
     return json.dumps(my_obj)
