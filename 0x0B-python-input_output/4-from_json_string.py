5cdd14ff289c50e50b83b36ced1c8f636b0802fa
Olofu Ojoachubione <87946002+Shugo52@users.noreply.github.com>
2023-02-08 07:45:08 +0100

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -4,4 +4,13 @@ import json
 
 
 def from_json_string(my_str):
+    """Makes a Python object from a JSON string
+
+    Args:
+        my_str: JSON string.
+
+    Returns:
+        A Python object. Can contain other python objects.
+    """
+
     return json.loads(my_str)
