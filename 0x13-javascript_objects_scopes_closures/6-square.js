32c671424530897dbdbc0d3cb6b777513a3f50a1
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-03-27 00:19:28 +0100

    Update on task  6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
-const SquareP = require('./5-square');
+const Nsquare = require('./4-rectangle');
 
-class Square extends SquareP {
+class Square extends Nsquare {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
+      this.print();
+    } else {
+      let i = 0;
+      while (i < this.height) {
+        console.log(c.repeat(this.width));
+        i++;
       }
-      console.log(s);
     }
   }
 }
