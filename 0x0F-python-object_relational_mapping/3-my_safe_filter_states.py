a6cfb30f9be9e1587969cdb71c240a3ec7400c4b
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-04-17 12:09:34 +0100

    Update' '

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+""" Filter states Module
+List all states in database (hbtn_0e_0_usa) and Safe from sql injections
+"""
+
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
     cur = db.cursor()
-    match = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (sys.argv[4], ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
