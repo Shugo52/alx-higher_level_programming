49c7384b15b46b1c9a8752fef2dffa975298888a
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-04-17 11:35:14 +0100

    Update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,9 +5,9 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name='{}'\
