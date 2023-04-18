918172f63a8badebe127ad45e882fce52dd5eb50
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-04-18 19:58:16 +0100

    update on file'

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+""" Change the naem of a state with id=2"""
 
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+import sys
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # Connect to mysqldb
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
+
+    # Begin a session
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_instance = session.query(State).filter_by(id=2).first()
-    new_instance.name = 'New Mexico'
+
+    # query to load first State instance that matches id=2
+    state = session.query(State).filter_by(id=2).first()
+
+    # Change name of state
+    state.name = "New Mexico"
+
+    # End and commit session
     session.commit()
