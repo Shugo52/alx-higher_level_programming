14cc9cf4d9f1c3648c1dc1efc021ba505daee046
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-04-18 12:05:37 +0100

    Update on model state fetch

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+""" Fetch from state Module"""
+
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # Connect to mysqldb
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
+
+    # Begin a session
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
+
+    # query to load State instances
+    for state in session.query(State).order_by(State.id):
+        print(str(state.id) + ': ' + str(state.name))
