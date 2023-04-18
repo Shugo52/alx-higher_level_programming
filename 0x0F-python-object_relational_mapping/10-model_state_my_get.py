d08acfa5b0bdfa617af77bedb9b36383ffe2234b
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-04-18 13:10:01 +0100

    Update on file

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+""" Fetch from state"""
 
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+import sys
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # Connect to mysqldb
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
+
+    # Begin a session
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4],))
+
+    # query to load State instance that matches a pattern
+    state = session.query(State).filter(State.name.like(f"%{sys.argv[4]}%"))
     try:
-        print(instance[0].id)
+        print(state[0].id)
     except IndexError:
         print("Not found")
