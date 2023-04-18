0e9b8f88778e212778d9573bf1be5fe23be0f018
Olofu Ojoachubione <87946002+Shugo52@users.noreply.github.com>
2023-04-18 21:27:47 +0100

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-mymetadata = MetaData()
+# mymetadata = MetaData()
 
 
 class State(Base):
